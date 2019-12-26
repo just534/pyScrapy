1ceb48b6e3c5f9c1fbafb2003e57c8f814e6307a
just534 <273510287@qq.com>
2019-12-26 15:33:00 +0800

    rednet.1

--- my_scrapy/pipelines.py
+++ my_scrapy/pipelines.py
@@ -8,4 +8,5 @@
 
 class MyScrapyPipeline(object):
     def process_item(self, item, spider):
+        print("!"*50)
         return item
--- my_scrapy/spiders/rednet.py
+++ my_scrapy/spiders/rednet.py
@@ -7,15 +7,11 @@ from scrapy.spiders import CrawlSpider, Rule
 class RednetSpider(CrawlSpider):
     name = 'rednet'
     allowed_domains = ['rednet.cn']
-    start_urls = ['http://rednet.cn/']
+    start_urls = ['https://news.rednet.cn/channel/8394.html']
 
     rules = (
-        Rule(LinkExtractor(allow=r'Items/'), callback='parse_item', follow=True),
+        Rule(LinkExtractor(allow=r'/channel/8394'), callback='parse_item', follow=True),
     )
 
     def parse_item(self, response):
-        item = {}
-        #item['domain_id'] = response.xpath('//input[@id="sid"]/@value').get()
-        #item['name'] = response.xpath('//div[@id="name"]').get()
-        #item['description'] = response.xpath('//div[@id="description"]').get()
-        return item
+        print(response.url)
